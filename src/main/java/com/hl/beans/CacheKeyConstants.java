83111f0c58ca51d225a18f2cd7349474fb0f0ec3
root <614772276@qq.com>
2019-03-22 09:17:27 +0800

    jsp

---
+++ src/main/java/com/hl/beans/CacheKeyConstants.java
@@ -0,0 +1,12 @@
+package com.hl.beans;
+
+import lombok.Getter;
+
+@Getter
+public enum CacheKeyConstants {
+
+    SYSTEM_ACLS,
+
+    USER_ACLS;
+
+}
---
+++ src/main/java/com/hl/service/SysUserService.java
@@ -0,0 +1,96 @@
+package com.hl.service;
+
+import com.google.common.base.Preconditions;
+import com.hl.beans.PageQuery;
+import com.hl.beans.PageResult;
+import com.hl.common.RequestHolder;
+import com.hl.dao.SysUserMapper;
+import com.hl.exception.ParamException;
+import com.hl.param.UserParam;
+import com.hl.pojo.SysUser;
+import com.hl.util.BeanValidator;
+import com.hl.util.IpUtil;
+import com.hl.util.MD5Util;
+import com.hl.util.PasswordUtil;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class SysUserService {
+
+    @Resource
+    private SysUserMapper sysUserMapper;
+    @Resource
+    private SysLogService sysLogService;
+
+    public void save(UserParam param) {
+        BeanValidator.check(param);
+        if(checkTelephoneExist(param.getTelephone(), param.getId())) {
+            throw new ParamException("电话已被占用");
+        }
+        if(checkEmailExist(param.getMail(), param.getId())) {
+            throw new ParamException("邮箱已被占用");
+        }
+        String password = PasswordUtil.randomPassword();
+        //TODO:
+        password = "12345678";
+        String encryptedPassword = MD5Util.encrypt(password);
+        SysUser user = SysUser.builder().username(param.getUsername()).telephone(param.getTelephone()).mail(param.getMail())
+                .password(encryptedPassword).deptId(param.getDeptId()).status(param.getStatus()).remark(param.getRemark()).build();
+        user.setOperator(RequestHolder.getCurrentUser().getUsername());
+        user.setOperateIp(IpUtil.getRemoteIp(RequestHolder.getCurrentRequest()));
+        user.setOperateTime(new Date());
+
+        // TODO: sendEmail
+        sysUserMapper.insertSelective(user);
+        sysLogService.saveUserLog(null, user);
+    }
+
+    public void update(UserParam param) {
+        BeanValidator.check(param);
+        if(checkTelephoneExist(param.getTelephone(), param.getId())) {
+            throw new ParamException("电话已被占用");
+        }
+        if(checkEmailExist(param.getMail(), param.getId())) {
+            throw new ParamException("邮箱已被占用");
+        }
+        SysUser before = sysUserMapper.selectByPrimaryKey(param.getId());
+        Preconditions.checkNotNull(before, "待更新的用户不存在");
+        SysUser after = SysUser.builder().id(param.getId()).username(param.getUsername()).telephone(param.getTelephone()).mail(param.getMail())
+                .deptId(param.getDeptId()).status(param.getStatus()).remark(param.getRemark()).build();
+        after.setOperator(RequestHolder.getCurrentUser().getUsername());
+        after.setOperateIp(IpUtil.getRemoteIp(RequestHolder.getCurrentRequest()));
+        after.setOperateTime(new Date());
+        sysUserMapper.updateByPrimaryKeySelective(after);
+        sysLogService.saveUserLog(before, after);
+    }
+
+    public boolean checkEmailExist(String mail, Integer userId) {
+        return sysUserMapper.countByMail(mail, userId) > 0;
+    }
+
+    public boolean checkTelephoneExist(String telephone, Integer userId) {
+        return sysUserMapper.countByTelephone(telephone, userId) > 0;
+    }
+
+    public SysUser findByKeyword(String keyword) {
+        return sysUserMapper.findByKeyword(keyword);
+    }
+
+    public PageResult<SysUser> getPageByDeptId(int deptId, PageQuery page) {
+        BeanValidator.check(page);
+        int count = sysUserMapper.countByDeptId(deptId);
+        if (count > 0) {
+            List<SysUser> list = sysUserMapper.getPageByDeptId(deptId, page);
+            return PageResult.<SysUser>builder().total(count).data(list).build();
+        }
+        return PageResult.<SysUser>builder().build();
+    }
+
+    public List<SysUser> getAll() {
+        return sysUserMapper.getAll();
+    }
+}
